a47d2ba0618b9f047bf934e2eed6db68b954baa4
Robert Lin <rcl2106@gmail.com>
2021-03-24 13:07:43 -0400

    ✨ Add MetaLogs

--- src/widgets/LogViewer.tsx
+++ src/widgets/LogViewer.tsx
@@ -1,10 +1,11 @@
 import styled, { css } from 'styled-components'
 import { FormattedDt, ILog } from './Shared'
 import { UserContext } from '../providers/AuthContext'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 interface IFlog {
   readonly background?: string
+  readonly type?: string
 }
 
 const FLogRecord = styled.div<IFlog>`
@@ -18,10 +19,22 @@ const FLogRecord = styled.div<IFlog>`
   ${props => props.background && css`
     background: ${props.background};
   `}
+
+  ${props => props.type === 'meta' && css`
+    height: 70px;
+    text-align: center;
+  `}
 `
 
-const LogRecord = ({ dt, activity, saved }: {dt: Date, activity: string, saved: boolean}) => {
-  const bg = saved ? 'darkblue' : '#00468b'
+const MetaLog = ({ content }: {content: string}) => {
+  return (
+    <FLogRecord background='purple' type='meta'>
+      {content}
+    </FLogRecord>
+  )
+}
+
+const ActivityLog = ({ dt, activity, bg }: {dt: Date, activity: string, bg: string}) => {
   return (
     <FLogRecord title={dt.toString()} background={bg}>
       {FormattedDt(dt)} :: {activity}
@@ -29,12 +42,81 @@ const LogRecord = ({ dt, activity, saved }: {dt: Date, activity: string, saved: 
   )
 }
 
+/**
+ * Loop through all raw logs once to add MetaLogs. Happens first.
+ * @param logs
+ * @returns
+ */
+function processLogs (logs: Array<ILog>): Array<any> {
+  // 1. Loop through all logs and add day breaks
+  const processedLogs = [] as any
+  let curDate = logs[0].dt
+  console.log('*******cur date:', curDate)
+
+  for (const log of logs) {
+    if (log.dt.toString().slice(0, 15) !== curDate.toString().slice(0, 15)) {
+      // Push a MetaLog into the array
+      const metaLog = {
+        type: 'MetaLog',
+        content: `A NEW DAY!! ${curDate.toString().slice(0, 15)}`
+      }
+      processedLogs.push(metaLog)
+      // processedLogs.push({ dt: curDate, activity: `A NEW DAY!! ${curDate.toString().slice(0, 15)}` })
+      curDate = log.dt
+    }
+
+    const activityLog = {
+      type: 'ActivityLog',
+      dt: log.dt,
+      activity: log.activity
+    }
+    processedLogs.push(activityLog)
+  }
+  return processedLogs // Return an array of RenderItems PLUS additional data.
+}
+
+/**
+ * Loop through all processed logs to generate the render markup. Happens second.
+ * @param logs
+ * @returns
+ */
+function renderLogs (items: Array<any>, bg: string): Array<any> {
+  console.log('{{{{}}}} logs first/last item:', items[0], items.slice(-1)[0])
+  const renderItems = [] as any
+  for (const [i, item] of items.entries()) {
+    if (item.type === 'MetaLog') {
+      renderItems.push(<MetaLog key={'ri' + i} {...item} />)
+    } else {
+      renderItems.push(<ActivityLog key={'ri' + i} {...item} bg={bg} />)
+    }
+  }
+  return renderItems
+}
+
 export const LogViewer = ({ logs }: {logs: Array<ILog>}) => {
   const { user } = useContext(UserContext)
+  const [pLogs, setPLogs] = useState([] as Array<any>) // processed logs
+
+  useEffect(() => {
+    if (logs.length > 0) {
+      if (user) {
+        const processedLogs = processLogs(logs)
+        setPLogs(renderLogs(processedLogs, 'darkblue')) // Saved logs
+      } else {
+        setPLogs(renderLogs(logs, '#00468b')) // Unsaved logs
+      }
+
+      // console.log('>> fired useEffect, pl:', pl)
+    } else {
+      setPLogs([])
+    }
+  }, [logs])
 
   return (
     <>
-      {logs.map((l: ILog, i: number) => <LogRecord key={'log' + i} {...l} saved={!!user} />)}
+      {pLogs}
+      {/* {pLogs.map((l: ILog, i: number) => <LogRecord key={'log' + i} {...l} saved={!!user} />)} */}
+      {/* {pLogs.map((l: ILog, i: number) => <LogRecord key={'log' + i} dt={l.dt} activity={l.activity} saved={!!user} />)} */}
     </>
   )
 }
