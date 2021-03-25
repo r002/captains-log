c71ab0994215fe14e7a8c9928d9b779ffbeca39b
Robert Lin <rcl2106@gmail.com>
2021-03-25 15:01:10 -0400

    🔧 Setup webpack.prod.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,17 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
   entry: {
     app: './src/index.tsx'
   },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    // inline: false,
-    port: 3000
-  },
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Production',
       template: './src/index.html',
       favicon: './src/favicon.ico'
     })
   ],
-  // plugins: [
-  //   // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-  //   new CleanWebpackPlugin(),
-  //   new HtmlWebpackPlugin({
-  //     title: 'Hot Module Replacement'
-  //   })
-  // ],
   module: {
     rules: [
       {
@@ -46,7 +31,8 @@ module.exports = {
     extensions: ['.tsx', '.ts', '.js']
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
   }
 }
