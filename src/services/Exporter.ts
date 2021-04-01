9fa924b73c725014b662301cb622c78b8acbfb84
Robert Lin <rcl2106@gmail.com>
2021-04-01 15:42:47 -0400

    ✨ #29: Impl log exporter - exports the most recent 1000 logs as '.json' file

--- src/services/Exporter.ts
+++ src/services/Exporter.ts
@@ -3,5 +3,12 @@ import { getLogs } from './FirestoreApi'
 export function exportLogs (limit: number) {
   getLogs(limit).then(logs => {
     console.log('>> number of logs to export:', logs.length)
+
+    const blob = new Blob([JSON.stringify(logs, null, 2) + '\n'], { type: 'text/plain' })
+    const url = window.URL.createObjectURL(blob)
+    const anchor = document.createElement('a')
+    anchor.href = url
+    anchor.download = `clog__${Date().slice(0, 21)}.json`
+    anchor.click()
   })
 }
