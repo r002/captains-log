c1e34d516ca38b563a0de8a6c05ee968b7195aec
Robert Lin <rcl2106@gmail.com>
2021-06-10 14:52:50 -0400

    🔖 1.7.0: 🍍 Begin `sprint-hala` (Thu - 6/10/21)

--- src/StudyGroup.tsx
+++ src/StudyGroup.tsx
@@ -427,7 +427,7 @@ const StudyGroup: React.FC<TStudyGroup> = (props) => {
         <a href='https://github.com/r002/codenewbie/issues'>Repo</a>&nbsp;&nbsp;&nbsp;
         <a href='https://github.com/r002/codenewbie/issues/4'>Members</a>&nbsp;&nbsp;&nbsp;
         <a href='https://github.com/r002/codenewbie/discussions/30?sort=new'>History</a>&nbsp;&nbsp;&nbsp;
-        <a href='https://github.com/r002/captains-log/blob/main/src/data/changelog.json'>Changelog</a>&nbsp;&nbsp;&nbsp;
+        <a href='https://github.com/r002/captains-log/blob/sprint-hala/src/data/changelog.json'>Changelog</a>&nbsp;&nbsp;&nbsp;
         <a href='https://community.codenewbie.org/r002/5-codenewbie-study-group-cohort-looking-for-study-mates-4lpj'>CodeNewbie</a>
       </FTopbarLinks>
       <div style={{ textAlign: 'center' }}>
