4dbcb56376e57adb28a701af5529384eb535515b
Robert Lin <rcl2106@gmail.com>
2021-08-05 14:03:05 -0400

    🔖 1.9.5: ✨ Add footer

--- src/StudyDash.tsx
+++ src/StudyDash.tsx
@@ -3,6 +3,7 @@ import './assets/css/board.css'
 import './assets/css/links+glyphs.css'
 import * as l from './widgets/Layout'
 import { MemberBoard } from './widgets/MemberBoard'
+import { Footer } from './widgets/Footer'
 
 import ReactDOM from 'react-dom'
 
@@ -10,7 +11,12 @@ ReactDOM.render(
   <l.Board>
     <MemberBoard />
   </l.Board>,
-  document.querySelector('#root')
+  document.getElementById('root')
+)
+
+ReactDOM.render(
+  <Footer />,
+  document.getElementById('footer')
 )
 
 // https://webpack.js.org/api/hot-module-replacement/
---
+++ src/widgets/Footer.tsx
@@ -0,0 +1,43 @@
+import changelogUri from '../data/changelog.json'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+const FFooter = styled.div`
+  text-align: right;
+  position: absolute;
+  color: black;
+  bottom: 0;
+  right: 0;
+  margin-bottom: 10px;
+  margin-right: 15px;
+  z-index: 99;
+  font-size: 14px;
+  line-height: 150%;
+`
+
+type Commit = {
+  version: string
+  message: string
+  built: Date
+}
+
+export const Footer: React.VFC = () => {
+  const [commit, setCommit] = useState<Commit>()
+
+  useEffect(() => {
+    fetch(changelogUri).then(rs => rs.json().then(payload => {
+      setCommit({
+        version: payload[0].version,
+        message: payload[0].message,
+        built: payload[0].built
+      })
+    }))
+  }, [])
+
+  return (
+    <FFooter>
+      🔖 {commit?.version} | {commit?.message}<br />
+      👷‍♂️ {commit?.built ? commit.built.toString() : 'NA'}
+    </FFooter>
+  )
+}
